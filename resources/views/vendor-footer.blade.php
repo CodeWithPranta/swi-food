c4e9469e34f9f687ba7be7d68c68faf36b6480d0
Pranta Mazumder <codewithpranta@gmail.com>
2025-08-07 00:16:06 +0600

    fix panel footer

--- resources/views/vendor-footer.blade.php
+++ resources/views/vendor-footer.blade.php
@@ -1,5 +1,5 @@
-<footer class="bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
-    <span class="text-sm text-gray-700 sm:text-center dark:text-gray-200">&copy;{{ date('Y')  }}
+<footer class="bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-900 dark:border-gray-700">
+    <span class="text-sm text-gray-700 sm:text-center dark:text-gray-200 mx-auto">&copy;{{ date('Y')  }}
         <a href="{{config('app.url', 'https://homestaurants.com')}}" class="hover:underline">{{config('app.name', 'Laravel')}}™</a>. All Rights Reserved.
     </span>
 </footer>
